cebc88fceda714990725f5acf770229e7f863c32
tomascalabria <totocalabria.tc@gmail.com>
2023-05-23 00:46:48 -0300

    color del home a rgb(255, 255, 255)

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -6,7 +6,7 @@ export const Home = () => {
   const { colorMode, toggleColorMode } = useColorMode();
 
   // Assign the background color based on colorMode outside of the JSX expression
-  const bgColor = colorMode ==='light' ? '#F5F5F5' : '#2A2A2A'
+  const bgColor = colorMode ==='light' ? 'rgb(255, 255, 255)' : '#2A2A2A'
 
   return (
     <Container bg={bgColor} w={'xl'} h={"xl"}>
